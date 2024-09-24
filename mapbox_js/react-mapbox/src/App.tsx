5429ed11ce761dd88d5dac07f8183ea020211f84
kaiton.ri <kaiton.ri@gmail.com>
2024-09-24 12:07:43 -0300

    Refactor map initialization and cleanup in App component; update CSS comment

--- mapbox_js/react-mapbox/src/App.tsx
+++ mapbox_js/react-mapbox/src/App.tsx
@@ -7,19 +7,23 @@ import './App.css'
 
 function App() {
 
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const mapRef = useRef<mapboxgl.Map | null>(null)
+  const mapContainerRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: [-74.0242, 40.6941],
-      zoom: 10.12
-    });
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic3BoeW54bGVlIiwiYSI6ImNtMWdrbDllbDA1ZTEycW9vMWhsamQ2NWsifQ.wAOKSfMchVOba8dGvboaPg'
+    if (mapContainerRef.current) {
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        center: [-74.0242, 40.6941],
+        zoom: 10.12
+      });
+    }
 
     return () => {
-      mapRef.current.remove()
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
     }
   }, [])
 
