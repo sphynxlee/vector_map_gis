8b07369464b3eafa48d959091cbabe86b4cc4afa
kaiton.ri <kaiton.ri@gmail.com>
2024-09-24 11:59:57 -0300

    Add Mapbox integration and update styles for map display

--- mapbox_js/react-mapbox/src/App.tsx
+++ mapbox_js/react-mapbox/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-74.0242, 40.6941],
+      zoom: 10.12
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id='map-container' ref={mapContainerRef}/>
     </>
   )
 }
