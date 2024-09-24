71d6050855f3b123983080378926387c252cbda7
kaiton.ri <kaiton.ri@gmail.com>
2024-09-24 13:42:52 -0300

    Refactor map initialization to use state for center and zoom; add constants for access token

--- mapbox_js/react-mapbox/src/App.tsx
+++ mapbox_js/react-mapbox/src/App.tsx
@@ -1,22 +1,38 @@
-import { useRef, useEffect } from 'react'
-import mapboxgl from 'mapbox-gl'
+import { useRef, useEffect, useState } from 'react'
+import mapboxgl, { LngLatLike } from 'mapbox-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import constants from './map-config/constants.json';
 import './App.css'
+
+const INITIAL_CENTER: LngLatLike = [-74.0242, 40.6941];
+const INITIAL_ZOOM: number = 10.12;
 
 function App() {
 
-  const mapRef = useRef<mapboxgl.Map | null>(null)
-  const mapContainerRef = useRef<HTMLDivElement | null>(null)
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const [center, setCenter] = useState<LngLatLike>(INITIAL_CENTER);
+  const [zoom, setZoom] = useState<number>(INITIAL_ZOOM);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic3BoeW54bGVlIiwiYSI6ImNtMWdrbDllbDA1ZTEycW9vMWhsamQ2NWsifQ.wAOKSfMchVOba8dGvboaPg'
+    mapboxgl.accessToken = constants.accessToken.mapbox;
     if (mapContainerRef.current) {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
-        center: [-74.0242, 40.6941],
-        zoom: 10.12
+        // center: [116.4, 39.9],
+        center: center,
+        zoom: zoom
+      });
+
+      mapRef.current.on('move', () => {
+        const mapCenter = mapRef.current?.getCenter();
+        const mapZoom = mapRef.current?.getZoom();
+
+        setCenter([mapCenter?.lng as number, mapCenter?.lat as number]);
+        setZoom(mapZoom as number);
       });
     }
 
@@ -25,10 +41,13 @@ function App() {
         mapRef.current.remove();
       }
     }
-  }, [])
+  }, [center, zoom]);
 
   return (
     <>
+      <div className="sidebar">
+        Longitude: {(center as number[])[0].toFixed(4)}, Latitude: {(center as number[])[1].toFixed(4)}, Zoom: {zoom.toFixed(2)}
+      </div>
       <div id='map-container' ref={mapContainerRef}/>
     </>
   )
